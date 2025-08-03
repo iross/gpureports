fc452d3717d5404295b68abdb11aabcc255a3a52
Ian Ross <iross@cs.wisc.edu>
2025-08-03 13:40:19 -0500

    update email list

--- emailer.sh
+++ emailer.sh
@@ -7,5 +7,5 @@ source .venv/bin/activate
 today=$(date +%Y-%m-%e)
 
 
-python usage_stats.py --output-format html --exclude-hosts-yaml masked_hosts.yaml --hours-back 24 --group-by-device | mailx -Smta=smtp://smtp.wiscmail.wisc.edu -Ssmtp-use-starttls \
-    -s "CHTC GPU Utilization $today" -r "iaross@wisc.edu" iaross@wisc.edu
+python usage_stats.py --exclude-hosts-yaml masked_hosts.yaml --hours-back 24 --group-by-device --email-to "chtc-reports@g-groups.wisc.edu,iaross@wisc.edu,gitter@biostat.wisc.edu"
+#python usage_stats.py --exclude-hosts-yaml masked_hosts.yaml --hours-back 24 --group-by-device --email-to "iaross@wisc.edu,BBockelman@morgridge.org,ckoch5@wisc.edu"
