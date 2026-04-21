28c952ab944fa2698f3b4b5e3c6509e2e676b36d
Ian Ross <iross@cs.wisc.edu>
2026-04-21 10:34:33 -0500

    add Joe

--- emailer.sh
+++ emailer.sh
@@ -7,5 +7,5 @@ source .venv/bin/activate
 today=$(date +%Y-%m-%e)
 
 
-python usage_stats.py --exclude-hosts-yaml masked_hosts.yaml --hours-back 24 --group-by-device --email-to "chtc-reports@g-groups.wisc.edu,iaross@wisc.edu,gitter@biostat.wisc.edu"
+python usage_stats.py --exclude-hosts-yaml masked_hosts.yaml --hours-back 24 --group-by-device --email-to "chtc-reports@g-groups.wisc.edu,iaross@wisc.edu,gitter@biostat.wisc.edu,jbart@cs.wisc.edu"
 #python usage_stats.py --exclude-hosts-yaml masked_hosts.yaml --hours-back 24 --group-by-device --email-to "iaross@wisc.edu,BBockelman@morgridge.org,ckoch5@wisc.edu"
